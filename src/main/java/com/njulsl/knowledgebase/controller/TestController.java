a7ce0d87128bfe2188f14e4c057d061cd45a7052
NjuLSL <522023320078@qq.com>
2024-03-20 22:06:41 +0800

    init spring

---
+++ src/main/java/com/njulsl/knowledgebase/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.njulsl.knowledgebase.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
+}
