a782e6c4b4692eb190e678900557a6abbb39dfc1
NjuLSL <522023320078@qq.com>
2024-03-20 22:06:29 +0800

    init spring

--- src/main/java/com/njulsl/knowledgebase/config/KnowledgeBaseApplication.java
+++ src/main/java/com/njulsl/knowledgebase/config/KnowledgeBaseApplication.java
@@ -1,8 +1,10 @@
-package com.njulsl.knowledgebase;
+package com.njulsl.knowledgebase.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.njulsl")
 @SpringBootApplication
 public class KnowledgeBaseApplication {
 
