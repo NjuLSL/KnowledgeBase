c466dd3cf90271e18b9ad8a8661638e2cc5038bc
NjuLSL <522023320078@qq.com>
2024-03-22 17:12:02 +0800

    commit

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    文档管理系统 ©Created by Nju_LSL
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
